169ee10c6783a5bd8c8c7070c351240f3fc21eef
Robert Ragans <robertragans@Roberts-MacBook-Pro-2.local>
2019-01-31 11:56:53 -0500

    added sign up functionality

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Domestico' });
 });
 
+router.post('/signupProcess', function(req, res, next) {
+  res.redirect('/login')
+});
+
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Domestico' });
 });
